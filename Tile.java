627d3a9cf21bf68ceee4ab38f08a9948e71a203d
ape ish <dyjasyza@gmail.com>
2026-07-12 20:22:32 +0800

    create enum Tile.java

---
+++ Tile.java
@@ -0,0 +1,19 @@
+/**
+ * Represents a tile within the dungeon floor. This will help determing whether
+ * Yohane can pass through a tile, and what happens if she interacts with it.
+ * 
+ * Tile
+ */
+
+//https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+
+public enum Tile {
+    PASSABLE, // .
+    WALL,     // v
+    SPIKE,    // x
+    WATER,    // w
+    HEAT,     // h
+    TREASURE, // T
+    EXIT,     // E
+    BORDER   //  *
+}
