e74032e63c5b65519fd24bb8459eadb8e167a4d9
acyviesca <amber_viesca@dlsu.edu.ph>
2026-08-02 18:39:28 +0800

    Update BossFight.java

--- BossFight.java
+++ BossFight.java
@@ -13,20 +13,44 @@ public class FinalBattle {
     private boolean exitSpawned;
 
 	public FinalBattle(Yohane yohane) {
-		this.yohane = yohane;
-		lailaps = new Lailaps;
-		siren = new Siren;
-		bats = new ArrayList<>();
-		switchesActivated = 0;
-		barriersBroken = false;
-		exitSpawned = false;
-    }
+				this.yohane = yohane;
+				lailaps = new Lailaps;
+				siren = new Siren;
+				bats = new ArrayList<>();
+				switchesActivated = 0;
+				barriersBroken = false;
+				exitSpawned = false;
+ }
 
 	public void displayBattle() {
 
 	} 
 
+/**
+* Moves Yohane and Lailaps
+*/
 	public void movePlayers() {
+			// read player input
+			String input = scanner.nextLine().toUpperCase();
+
+			if (input.isEmpty())
+						return;
+
+			char move = input.charAt(0);
+
+			//move Yohane
+			moveYohane(move);
+
+			//move Lailaps
+			moveLailaps(move);
+
+			// count player move
+			moveCounter++;
+}
+
+		
+
+
 
 	}
 
