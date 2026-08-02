8cd4ae058bc1c30d7d6f5bcebc68690e3e981471
acyviesca <amber_viesca@dlsu.edu.ph>
2026-08-02 21:22:15 +0800

    Update BossFight.java

--- BossFight.java
+++ BossFight.java
@@ -14,10 +14,15 @@ public class FinalBattle {
 
 	public FinalBattle(Yohane yohane) {
 				this.yohane = yohane;
-				lailaps = new Lailaps;
-				siren = new Siren;
+
+				lailaps = new Lailaps(startRow,startCol);
+				siren = new Siren(startRow,startCol);
+
 				bats = new ArrayList<>();
+
 				switchesActivated = 0;
+				moveCounter = 0;
+
 				barriersBroken = false;
 				exitSpawned = false;
  }
@@ -38,20 +43,74 @@ public class FinalBattle {
 
 			char move = input.charAt(0);
 
-			//move Yohane
-			moveYohane(move);
+			switch (move) {
+        case 'W':
+            movePlayer(-1, 0);
+            moveLailaps(-1, 0);
+            break;
 
-			//move Lailaps
-			moveLailaps(move);
+        case 'S':
+            movePlayer(1, 0);
+            moveLailaps(1, 0);
+            break;
 
+        case 'A':
+            movePlayer(0, -1);
+            moveLailaps(0, -1);
+            break;
+
+        case 'D':
+            movePlayer(0, 1);
+            moveLailaps(0, 1);
+            break;
+
+        default:
+            return;
+			}
 			// count player move
 			moveCounter++;
 }
 
-		
 
+/**
+* Handles Lailaps' movement
+*/
+public void moveLailaps(int rowChange, int colChange) {
 
+    int newRow = lailaps.getRow() + rowChange;
+    int newCol = lailaps.getCol() + colChange;
 
+    if (!isInsideMap(newRow, newCol))
+        return;
+
+    Tile target = map[newRow][newCol];
+
+    if (!target.isPassable)
+        return;
+
+    lailaps.setPosition(newRow, newCol);
+}
 	}
+
+private void checkSwitches() {
+				// check if Yohane is on her switch
+    boolean yohaneOnSwitch = isYohaneOnSwitch();
+
+    // check if Lailaps is on his switch
+    boolean lailapsOnSwitch = isLailapsOnSwitch();
+
+    // both switches activated
+    if (yohaneOnSwitch && lailapsOnSwitch) {
+        switchesActivated++;
+        // remove the barriers after the third activation
+        if (switchesActivated == 3) {
+            barriersBroken = true;
+        }
+
+        // otherwise generate another pair of switches
+        else {
+            spawnSwitches();
+        }
+}
 
 }
